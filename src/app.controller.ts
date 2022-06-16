a3dd0064a07e022b37f6af442811c3b9f4fc40ca
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-06-16 10:31:08 +0200

    user module added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // POST /login
+  @Post('login')
+  login(): any {
+    return 'Hallo Login';
+  }
 
   @Get()
   getHello(): string {
